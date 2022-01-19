b62a84ff9628f52628f1ceec87f678a9d3d7580b
alex <gromvolk97@gmail.com>
2022-01-19 14:48:03 +0300

    feature(catalog): added category card

---
+++ src/components/c3-catalog/data.js
@@ -0,0 +1,28 @@
+import {v4 as uuidv4} from 'uuid'
+import img1 from '../../assets/img/catrgory/1.jpg'
+import img2 from '../../assets/img/catrgory/2.jpg'
+import img3 from '../../assets/img/catrgory/3.jpg'
+import img4 from '../../assets/img/catrgory/4.jpg'
+import img5 from '../../assets/img/catrgory/5.jpg'
+import img6 from '../../assets/img/catrgory/6.jpg'
+import img7 from '../../assets/img/catrgory/7.jpg'
+import img8 from '../../assets/img/catrgory/8.jpg'
+import img9 from '../../assets/img/catrgory/9.jpg'
+import img10 from '../../assets/img/catrgory/10.jpg'
+import img11 from '../../assets/img/catrgory/11.jpg'
+import img12 from '../../assets/img/catrgory/12.jpg'
+
+export const cardItems = [
+    {img: img1, title: 'Романтические', id: uuidv4()},
+    {img: img2, title: 'Детские', id: uuidv4()},
+    {img: img3, title: 'Животные', id: uuidv4()},
+    {img: img4, title: 'Фото-светильники', id: uuidv4()},
+    {img: img5, title: 'Техника', id: uuidv4()},
+    {img: img6, title: 'MARVEL/DC', id: uuidv4()},
+    {img: img7, title: 'Герои и персонажи', id: uuidv4()},
+    {img: img8, title: 'Космос', id: uuidv4()},
+    {img: img9, title: 'Спорт', id: uuidv4()},
+    {img: img10, title: 'Эмаблемы команд', id: uuidv4()},
+    {img: img11, title: '3D-фигуры', id: uuidv4()},
+    {img: img12, title: 'Интерьер/Дизайн', id: uuidv4()},
+]
--- src/components/c3-catalog/index.jsx
+++ src/components/c3-catalog/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import s from './style.module.scss'
+import {Button, Card} from "react-bootstrap";
+import {cardItems} from "./data";
+import Container from "../../common/container/container";
 
 const Catalog = () => {
+    const cardItem = cardItems.map((el) => {
+        return (
+            <Card style={{width: '18rem'}} key={el.id} className={s.items}>
+                <Card.Img className={s.items_img} variant="top" src={el.img}/>
+                <Card.Body className={s.items_body}>
+                    <Card.Title>{el.title}</Card.Title>
+                </Card.Body>
+            </Card>
+        )
+    })
+
     return (
         <div className={s.catalog}>
-            <h1>catalog</h1>
+            <h1>Выберите подходящую для вас категорию</h1>
+            <div className={s.items_container}>
+                {cardItem}
+            </div>
         </div>
     );
 };
